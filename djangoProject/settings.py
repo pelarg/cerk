7e202e55efc8ef01dba3c77fd8fd31134c5ee9cf
Pelarg <pelargonia52022@gmail.com>
2024-09-23 16:09:29 +0700

    день 235

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-btzd4y#lk*#9l$986a*ok#)x34-zpfd-l&%vrta_&y^us00i=q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://github.com/pelarg/cerk.git']
 
 
 # Application definition
