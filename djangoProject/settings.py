cce0b95ce3bc6a9afa6dbdece5771a89c3033c4b
Pelarg <pelargonia52022@gmail.com>
2024-09-29 20:08:42 +0700

    F

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -124,6 +124,7 @@ STATIC_URL = "/staticfiles/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+STATIC_ROOT=BASE_DIR / 'staticfiles'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
