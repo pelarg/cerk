dc3cec4b679ef135604f8981dabd6d4e5ff00241
Pelarg <pelargonia52022@gmail.com>
2024-09-30 15:42:26 +0700

    FF

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-btzd4y#lk*#9l$986a*ok#)x34-zpfd-l&%vrta_&y^us00i=q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
