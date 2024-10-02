0d9ee53a3b0ebb9503f5d66923eb49aed100b8a3
Pelarg <pelargonia52022@gmail.com>
2024-10-02 08:19:31 +0700

    Bugs2

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,8 @@ from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
+    path('', views.mobile_version, name='mobile_version')
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- main/views.py
+++ main/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from user_agents import parse
 
 def home(request):
     return render(request, 'main/home.html', {})
+
+def mobile_version(request):
+    user_agent = parse(request.META['HTTP_USER_AGENT'])
+    is_mobile = user_agent.is_mobile
+
+    if is_mobile:
+        return render(request, 'main/mobile_version.html')
+    else:
+        return render(request, 'main/home.html')
