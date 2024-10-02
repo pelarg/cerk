c484df43867fb1e9c4020982533a9030b513d90a
Pelarg <pelargonia52022@gmail.com>
2024-10-02 19:36:16 +0700

    7

--- main/views.py
+++ main/views.py
@@ -5,7 +5,13 @@ def home(request):
     return render(request, 'main/home.html', {})
 
 def mobile_version(request):
-    user_agent = parse(request.META['HTTP_USER_AGENT'])
+    user_agent_string = request.META.get('HTTP_USER_AGENT', '')
+
+    if not user_agent_string:
+        # Обработка случая, если User-Agent не найден
+        return render(request, 'main/home.html')
+
+    user_agent = parse(user_agent_string)
     is_mobile = user_agent.is_mobile
 
     if is_mobile:
