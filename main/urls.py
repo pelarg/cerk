658667d10eec48c24a6108a3f878252b55ee2250
Pelarg <pelargonia52022@gmail.com>
2024-10-03 18:07:07 +0700

    k

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('home/', views.home, name='home'),
-    path('', views.mobile_version, name='mobile_version')
+    path('', views.mobile_version, name='mobile_version'),
+    path('app2/', include('app2.urls')),
+    path('app1/', include('app1.urls'))
 ]
 
 urlpatterns += staticfiles_urlpatterns()
